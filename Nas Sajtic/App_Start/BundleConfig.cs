547fa4070624d349b05eaf0539554abe37172ea9
MirkoLisanin <mirkolisanin@gmail.com>
2018-04-23 17:08:08 +0200

    23.04.2018.

--- Nas Sajtic/App_Start/BundleConfig.cs
+++ Nas Sajtic/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace Nas_Sajtic
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/mikroMars_global.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
